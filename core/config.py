e4a6df204aa29a721cf33757954b7078a3325b61
Herfando <herfando_91@yahoo.com>
2026-04-09 09:53:34 +0700

    modified:   core/config.py

--- core/config.py
+++ core/config.py
@@ -1,12 +1,11 @@
-import os
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://user:pass@localhost:5432/dbname")
-    REDIS_URL: str = os.getenv("REDIS_URL", "redis://localhost:6379/0")
-    SECRET_KEY: str = os.getenv("SECRET_KEY", "supersecret")
+    DATABASE_URL: str
+    REDIS_URL: str
+    SECRET_KEY: str
 
-class Config:
+    class Config:
         env_file = ".env"
 
 settings = Settings()
