e6644d1a1976055fca5a8954d7d5472ba85baa50
Herfando <herfando_91@yahoo.com>
2026-04-10 10:04:22 +0700

    modified:   main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
-from api import users
+from api import 
+from code.logging_config import configure_logging
 
+setup_logging()
 app = FastAPI(title="Acumen Backend")
 
 app.include_router(users.router)
